2f3eb0fe998b9e1d9e5d732bf7b6f44005c97a85
yjkim <yjkim@aimmed.com>
2023-11-02 10:04:05 +0900

    [FEAT] layout & nav link

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+
+// 모든 페이지에서 공유됨
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
-import AcmeLogo from '@/app/ui/acme-logo';
+import AcmeLogo from "@/app/ui/acme-logo";
+import styles from "@/app/ui/home.module.css";
 
-import Link from 'next/link';
+import Link from "next/link";
+import { lusitana } from "./ui/fonts";
+import Image from "next/image";
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -9,8 +12,11 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+          <div className={styles.shape}></div>
+          <p
+            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+          >
+            <strong>Welcome to Acme.</strong> This is the example for the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
@@ -25,6 +31,10 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <img
+            src="/hero-desktop.png"
+            alt="Screenshots of the dashboard project showing desktop and mobile versions"
+          />
         </div>
       </div>
     </main>
